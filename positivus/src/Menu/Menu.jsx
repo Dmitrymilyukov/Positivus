5302ea2978a5ee3db1d3d4afea84e2afc89d243a
Dmitrymilyukov <145593337+Dmitrymilyukov@users.noreply.github.com>
2025-04-03 17:31:49 +0300

    add file

---
+++ positivus/src/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <>
+      <div className="container">
+        <div>
+            <img src="" alt="" />
+        </div>
+        <nav className="nav">
+          <a className="nav_a" href="">About us</a>
+          <a className="nav_a" href="">Services</a>
+          <a className="nav_a" href="">Us cases</a>
+          <a className="nav_a" href="">Princing</a>
+          <a className="nav_a" href="">Blog</a>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Menu;
